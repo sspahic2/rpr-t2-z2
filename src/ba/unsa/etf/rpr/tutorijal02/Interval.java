2aa0326090f857c6583a184bd96d3d4debea8203
demon034 <sspahic2@etf.unsa.ba>
2019-10-25 15:45:44 +0200

    Drugi commit napravio metode i konstruktore, samo fali intersect i da preklopim neke metode

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,48 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetna, krajnja;
+    boolean pocPripada, kraPripada;
 
+    Interval(double pocetna, double krajnja, boolean pripada1, boolean pripada2) {
+        if(pocetna > krajnja)
+            throw new IllegalArgumentException("Pocetna veca od krajnje!");
 
+        this.pocetna = pocetna;
+        this.krajnja = krajnja;
+        pocPripada = pripada1;
+        kraPripada = pripada2;
+    }
+
+    Interval() {
+        pocetna = 0;
+        krajnja = 0;
+        pocPripada = false;
+        kraPripada = false;
+    }
+
+    boolean isNull() {
+        return (pocetna == 0 && pocetna == krajnja && !pocPripada && pocPripada == kraPripada);
+    }
+
+    boolean isIn(double tacka) {
+        return tacka >= pocetna && tacka <= krajnja;
+    }
+
+    Interval intersect(Interval interval) {
+        boolean jedan = true, dva = true;
+        double poc, kra;
+
+        if(this.pocetna > interval.pocetna) {
+            poc = pocetna;
+            jedan = interval.pocPripada;
+        }
+        else {
+            poc = interval.pocetna;
+        }
+    }
+
+    Interval intersect(Interval prvi, Interval drugi) {
+        if(prvi.pocetna > )
+    }
 }
